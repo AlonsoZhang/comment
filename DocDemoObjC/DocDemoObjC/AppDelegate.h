4e1903057b46367c61eeb98ae71ed4879d67585a
Alonso <code@zhangwu.space>
2016-08-17 08:23:04 +0800

    Init DocDemoObjC project

---
+++ DocDemoObjC/DocDemoObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DocDemoObjC
+//
+//  Created by Alonso Zhang on 16/8/17.
+//  Copyright © 2016年 Alonso Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DocDemoObjC/DocDemoObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DocDemoObjC
+//
+//  Created by Alonso Zhang on 16/8/17.
+//  Copyright © 2016年 Alonso Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
